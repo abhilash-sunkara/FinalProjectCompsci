be7c405891fb7b10930dc0aa2d6fda98ec06acdd
abhilash-sunkara <52720913+abhilash-sunkara@users.noreply.github.com>
2024-04-19 11:18:04 -0500

    Physics + Collisions

--- core/src/com/mygdx/game/Plane.java
+++ core/src/com/mygdx/game/Plane.java
@@ -1,39 +1,36 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.mygdx.game.Characters.BluePlane;
 import com.mygdx.game.Characters.BluePlaneSprite;
-import com.mygdx.game.Characters.EnemyPlaneSprite;
+import com.mygdx.game.Managers.EnemyManager;
 
 public class Plane extends ApplicationAdapter {
 	SpriteBatch batch;
 
 	OrthographicCamera camera;
 
-	//private BluePlane player = new BluePlane();
 	String bluePlaneImg = "ship_0000.png";
-	String enemyPlaneImg = "ship_0001.png";
 	BluePlaneSprite planeSprite;
-	EnemyPlaneSprite enemyPlaneSprite;
+	World world;
 	EnemyManager boss;
+	Box2DDebugRenderer debugRenderer;
 	
 	@Override
 	public void create () {
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 800, 480);
 		batch = new SpriteBatch();
-		planeSprite = new BluePlaneSprite(bluePlaneImg, batch);
-		enemyPlaneSprite = new EnemyPlaneSprite(enemyPlaneImg, batch);
-		boss = new EnemyManager(batch);
-		boss.create();
-		//player.create(bluePlaneImg);
+		world = new World(new Vector2(0, 0), true);
+
+		planeSprite = new BluePlaneSprite(bluePlaneImg, batch, world);
+		boss = new EnemyManager(batch, world);
+		debugRenderer = new Box2DDebugRenderer();
 	}
 
 	@Override
@@ -41,8 +38,13 @@ public class Plane extends ApplicationAdapter {
 		ScreenUtils.clear(1, 1, 1, 1);
 		batch.begin();
 		planeSprite.update();
-		enemyPlaneSprite.update();
 		boss.update();
+		world.step(1/60f, 6, 2);
+		world.step(1/60f, 6, 2);
+		world.step(1/60f, 6, 2);
+		world.step(1/60f, 6, 2);
+		world.step(1/60f, 6, 2);
+		//debugRenderer.render(world, camera.combined);
 		batch.end();
 	}
 	
--- core/src/com/mygdx/game/Projectiles/EnemyBullet.java
+++ core/src/com/mygdx/game/Projectiles/EnemyBullet.java
@@ -1,14 +1,13 @@
-package com.mygdx.game.Characters;
+package com.mygdx.game.Projectiles;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.physics.box2d.*;
 
-public class Bullet {
-    //initial push
+public class EnemyBullet {
+
     public Sprite sprite;
     public SpriteBatch renderer;
     public boolean isActive = true;
@@ -17,7 +16,7 @@ public class Bullet {
     private Body body;
     private Fixture f;
 
-    public Bullet(String imgFile, SpriteBatch spriteBatch, World world){
+    public EnemyBullet(String imgFile, SpriteBatch spriteBatch, World world){
         sprite = new Sprite(new Texture(Gdx.files.internal(imgFile)));
         renderer = spriteBatch;
         bd.type = BodyDef.BodyType.DynamicBody;
@@ -27,62 +26,27 @@ public class Bullet {
         body.setLinearDamping(2.0f);
 
         CircleShape cs = new CircleShape();
-        cs.setRadius(32f);
+        cs.setRadius(6f);
         FixtureDef fd = new FixtureDef();
         fd.shape = cs;
         Fixture fixture = body.createFixture(fd);
         fixture.setUserData(this);
-        //fixture.setFilterData(new Filter());
+        f = fixture;
     }
 
-    /*
     public void bulletMovement(){
-        sprite.translateY(8);
-        if(sprite.getY() > 300){
-            isActive = false;
-        }
-    }
-    */
-
-
-    public void bulletMovement(){
-        body.setLinearVelocity(0f, 100f);
-        if(sprite.getY() > 500){
-            isActive = false;
-        }
+        body.setLinearVelocity(0, -80);
         sprite.setPosition(body.getPosition().x, body.getPosition().y);
     }
 
     public void update(){
-        //System.out.println(f);
         if(isActive){
-            //planeMovement();
             bulletMovement();
             sprite.draw(renderer);
         }
     }
 
-    /*
-    public void planeMovement(){
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-            body.applyForceToCenter(1000.0f, 0f, true);
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            body.applyForceToCenter(-1000.0f, 0f, true);
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.UP) ) {
-            body.applyForceToCenter(0f, 1000.0f, true);
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)){
-            body.applyForceToCenter(0f, -1000.0f, true);
-        }
-        //System.out.println(planeBody.getPosition().x);
-        sprite.setPosition(body.getPosition().x, body.getPosition().y);
-    }
-    */
-
-
-    public Bullet setPosition(float x, float y){
+    public EnemyBullet setPosition(float x, float y){
         sprite.setPosition(x, y);
         body.setTransform(x, y, 0);
         return this;
@@ -90,6 +54,9 @@ public class Bullet {
 
     public void destroy(){
         isActive = false;
+        f.setSensor(true);
     }
+
+
 
 }
