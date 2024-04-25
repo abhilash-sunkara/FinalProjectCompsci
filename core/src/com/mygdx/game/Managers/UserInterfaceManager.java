e5ce535305659d9a71051c82956b668b0593125d
abhilash-sunkara <52720913+abhilash-sunkara@users.noreply.github.com>
2024-04-25 14:53:56 -0500

    Player life meter update

--- core/src/com/mygdx/game/Managers/UserInterfaceManager.java
+++ core/src/com/mygdx/game/Managers/UserInterfaceManager.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeType.Bitmap;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 import com.badlogic.gdx.utils.Array;
